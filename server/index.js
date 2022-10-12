f3745df8c7f8262500f0da852b9d90e44601f419
Ruslan Mavletkulov <mavletkulov@rusapr.ru>
2022-10-12 00:12:36 -0400

    Prtoject & client queries

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const colors = require('colors')
+
 require('dotenv').config()
 const schema = require('./schema/schema')
 
 const { graphqlHTTP } = require('express-graphql')
+const connectDB = require('./config/db')
 
 const port = process.env.PORT || 5000
 
 const app = express()
+
+connectDB()
 
 app.use(
   '/graphql',
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,10 @@
 const { projects, clients } = require('../sampleData.js')
 
+// Mongoose models
+
+const Project = require('../models/Project')
+const Client = require('../models/Client')
+
 const {
   GraphQLObjectType,
   GraphQLID,
@@ -20,7 +25,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId)
+        return Client.findById(parent.clientId)
       },
     },
   }),
@@ -44,27 +49,27 @@ const RootQuery = new GraphQLObjectType({
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects
+        return Project.find()
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id)
+        return Project.findById(args.id)
       },
     },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients
+        return Client.find()
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id)
+        return Client.findById(args.id)
       },
     },
   },
