a0b336f9f0eb4ce51a7067f9d545679773a3a090
Ruslan Mavletkulov <mavletkulov@rusapr.ru>
2022-10-26 17:46:49 -0400

    add new project

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import Header from './components/Header'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-import Clients from './components/Clients'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Project from './pages/Project'
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -30,10 +33,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className='container'>
-          <Clients />
-        </div>
+        <BrowserRouter>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/project/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </ApolloProvider>
     </>
   )
